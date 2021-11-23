a904ede748be1ac4b508c7026268bb6aad67cd08
Büşra Yeşilalioğlu <88002831+7Auri@users.noreply.github.com>
2021-11-24 00:55:12 +0300

    footera grid eklendi

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,11 +1,33 @@
 import React from 'react'
+import GridLayout from 'react-grid-layout';
 
 function Footer() {
-    return (
-        <div>
-            Footer
-        </div>
-    )
-}
+ 
+    const layout = [
+        {i: 'a', x: 0, y: 0, w: 3, h: 4, static: true},
+        {i: 'b', x: 4, y: 0, w: 3, h: 4, static:true},
+        {i: 'c', x: 8, y: 0, w: 3, h: 4, static:true}]
 
-export default Footer
+    return (
+        <div className="footer">
+           Created By:...
+           <GridLayout className="layout" layout={layout} cols={12} rowHeight={30} width={1200}>
+               
+        <div className="layout1" key="a"  >  <a href="http://tympanus.net/codrops/2014/01/14/look-inside-book-preview-with-bookblock/" target="_blank" rel="noreferrer">
+        <p className="yazi">
+             Book Preview</p>
+				</a></div>
+        <div className="layout2" key="b"><a href="https://reactjs.org/" target="_blank" rel="noreferrer">
+        <p className="yazi">React Js</p>
+				</a></div>
+        <div className="layout3" key="c" >  <a href="https://developers.google.com/books/?hl=en" target="_blank" rel="noreferrer"> 
+       <p className="yazi">Google Books API</p>
+				</a></div>
+        
+      </GridLayout>
+
+
+        </div>
+         
+    )
+}export default Footer
