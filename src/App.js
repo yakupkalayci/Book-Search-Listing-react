c7e8f920572522fd1fe6219de4276d94f3533b3f
Büşra Yeşilalioğlu <88002831+7Auri@users.noreply.github.com>
2021-11-24 01:01:51 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Header from "./components/Header";
-import Card from "./components/Card";
+
 import Footer from "./components/Footer";
 
 import { BookSearchContextProvider } from "./context/BookSearchContext";
